6e42875a53811d10cdadab813ac26aa1c3630b0e
Jacob-M-Webster <jakewebster5619@gmail.com>
2025-09-27 01:08:20 -0400

    Adde routing, turned app.vue into just the geneal template of what very page should be (needs to be changed), moved app.vue contents into home.vue

---
+++ frontend/src/pages/home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="p-8 text-center">
+    <h1 class="text-2xl font-bold mb-4">CONNECTED</h1>
+    <p v-if="message" class="text-green-600 text-lg">
+      {{ message }}
+    </p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      message: null,
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:8080/api/hello")
+      .then(response => {
+        this.message = response.data.message;
+      })
+      .catch(error => {
+        console.error("Error fetching from backend:", error);
+      });
+  }
+};
+</script>
+
+<style>
+</style>
