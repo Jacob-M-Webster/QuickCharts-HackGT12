8065d5e49ebdfcecdab571314b7c4a614f692328
Jacob-M-Webster <jakewebster5619@gmail.com>
2025-09-28 03:40:36 -0400

    fixed origin issue

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -19,7 +19,7 @@ public class BackendApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")      // allow all endpoints
-                        .allowedOrigins("http://localhost:5176", "http://localhost:5173"); // your Vue dev server
+                        .allowedOrigins("http://localhost:5176", "http://localhost:5173", "http://localhost:5175", "http://localhost:5174"); // your Vue dev server
                         
             }
         };
