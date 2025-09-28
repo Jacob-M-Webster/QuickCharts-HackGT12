e42b641cb740253c4e25aefb82f892b432872561
michelleyi23 <Yenabanana94@gmail.com>
2025-09-28 00:29:55 -0400

    Edit the registration page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import HomePage from '../pages/home.vue';
 import LoginPage from '../pages/login.vue';
 import LogEdit from '../pages/logedit.vue';
 import RegistrationPage from '../pages/register.vue';
+import RegEdit from '../pages/regedit.vue';
 import ProfilePage from '../pages/profile.vue';
 import About from "../pages/about.vue";
 import ServicePage from '../pages/service.vue';
@@ -12,7 +13,7 @@ import SchedulePage from '../pages/schedule.vue'; // Make sure this is imported
 const routes = [
     { path: '/', name: 'Home', component: HomePage },
     { path: '/login', name: 'Login', component: LogEdit }, // use your new login page
-    { path: '/registration', name: 'Register', component: RegistrationPage },
+    { path: '/registration', name: 'Register', component: RegEdit },
     { path: '/profile', name: 'Profile', component: ProfilePage },
     { path: '/about', name: 'About', component: About },
     { path: '/services', name: 'Services', component: ServicePage },
